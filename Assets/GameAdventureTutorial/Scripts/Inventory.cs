d157c78584dff0c2b532b05bce2e16736b6624b3
swaroop-sapkota <swaroopsapkota1@gmail.com>
2025-03-26 17:57:50 -0500

    Use inventory items
    Use inventory items

--- Assets/GameAdventureTutorial/Scripts/Inventory.cs
+++ Assets/GameAdventureTutorial/Scripts/Inventory.cs
@@ -6,15 +6,21 @@ using UnityEngine.UI;
 public class Inventory : MonoBehaviour {
 
     public GameObject invTab;
+    //
+    private bool isOpen = false;
 
 	// Use this for initialization
 	void Start () {
         invTab.SetActive(false);
+        //
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
+        /*
         if (Input.GetKey(KeyCode.Tab))
         {
             invTab.SetActive(true);
@@ -25,5 +31,29 @@ public class Inventory : MonoBehaviour {
             invTab.SetActive(false);
             Cursor.visible = false;
         }
+        */
+        if(Input.GetKeyDown(KeyCode.Tab))
+        {
+            ToggleInventory();
+        }
 	}
+
+    void ToggleInventory()
+    {
+        isOpen = !isOpen;
+        invTab.SetActive(isOpen);
+
+        if(isOpen)
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+            Time.timeScale = 1f;
+        }
+    }
 }
